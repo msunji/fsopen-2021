a134d74d63479a8875781aa1ca173ee7bd9d8a8f
Marge <marge.consunji@gmail.com>
2021-04-01 14:48:44 +0800

    Setting up for Unicafe exercises

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+function App() {
+  // State
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+
+  return (
+    <div>
+      Setting this up
+    </div>
+  );
+}
+
+export default App;
