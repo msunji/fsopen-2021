1ed83773b25d784b912dc33b8c744f744dba70a1
Marge <marge.consunji@gmail.com>
2021-04-01 15:24:54 +0800

    Finished Ex 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,14 +1,47 @@
 import React, { useState } from 'react';
 
-function App() {
+const Button = ({ handleClick, text }) => {
+  console.log('click button');
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
   // State
   const [good, setGood] = useState(0);
   const [bad, setBad] = useState(0);
   const [neutral, setNeutral] = useState(0);
 
+  // Event Handlers
+
+  const handleGood = () => {
+    setGood(good + 1);
+  }
+
+  const handleBad = () => (
+    setBad(bad + 1)
+  );
+
+  const handleNeutral = () => (
+    setNeutral(neutral + 1)
+  );
+
   return (
     <div>
-      Setting this up
+      <h1>give feedback</h1>
+      <div>
+        <Button handleClick={handleGood} text="good" />
+        <Button handleClick={handleNeutral} text="neutral" />
+        <Button handleClick={handleBad} text="bad" />
+      </div>
+
+      <h1>statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
   );
 }
