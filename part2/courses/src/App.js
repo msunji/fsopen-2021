e51132cd1dd81333a58444bb32fe8cf3ff44fb86
Marge <marge.consunji@gmail.com>
2021-04-02 16:42:39 +0800

    Completed Exercise 2.1. Show Course name and details, etc

--- part2/courses/src/App.js
+++ part2/courses/src/App.js
@@ -1,68 +1,80 @@
-import React from 'react';
+import React from "react";
 
 // Header component
-const Header = ({course}) => {
+const Header = ({ course }) => {
   return <h1>{course.name}</h1>;
-}
+};
 
 // // Part component
-const Part = ({name, exercises}) => {
+const Part = ({ name, exercises }) => {
   console.log(name, exercises);
-  return <p>{name} {exercises}</p>
-}
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
 
 //Content component
-const Content = ({course}) => {
+const Content = ({ course }) => {
   const courseParts = course.parts;
   return (
     <div>
-      {courseParts.map(part => <Part key={part.name} name={part.name} exercises={part.exercises} />)} 
+      {courseParts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-// Total component
-const Total = ({course}) => {
-  const courseParts = course.parts;
+// // Total component
+// const Total = ({course}) => {
+//   const courseParts = course.parts;
 
-  return (
-    <p>
-      Number of exercises: {
-        courseParts.reduce((accumulator, part) => accumulator + part.exercises, 0)
-      }
-    </p>
-  )
-}
+//   return (
+//     <p>
+//       Number of exercises: {
+//         courseParts.reduce((accumulator, part) => accumulator + part.exercises, 0)
+//       }
+//     </p>
+//   )
+// }
 
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
+// Course component
+const Course = ({ course }) => {
+  console.log(course);
   return (
     <div>
       <Header course={course} />
-      <Content 
-        course={course}
-      />
-      <Total course={course} />
+      <Content course={course} />
     </div>
-  )
-}
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
 
 export default App;
