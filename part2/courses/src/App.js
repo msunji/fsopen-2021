efe839f8fd46d8dab93fcefe3083b3f00d68c5ee
Marge <marge.consunji@gmail.com>
2021-04-02 16:55:24 +0800

    Map thorugh courses object and show course names

--- part2/courses/src/App.js
+++ part2/courses/src/App.js
@@ -5,82 +5,111 @@ const Header = ({ course }) => {
   return <h1>{course.name}</h1>;
 };
 
-// // Part component
-const Part = ({ name, exercises }) => {
-  console.log(name, exercises);
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  );
-};
+// // // Part component
+// const Part = ({ name, exercises }) => {
+//   console.log(name, exercises);
+//   return (
+//     <p>
+//       {name} {exercises}
+//     </p>
+//   );
+// };
 
 //Content component
 const Content = ({ course }) => {
-  const courseParts = course.parts;
-  return (
-    <div>
-      {courseParts.map((part) => (
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
+  console.log(course);
+  return <Header course={course} />;
 };
+// const Content = ({ course }) => {
+//   const courseParts = course.parts;
+//   return (
+//     <div>
+//       {courseParts.map((part) => (
+//         <Part key={part.id} name={part.name} exercises={part.exercises} />
+//       ))}
+//     </div>
+//   );
+// };
 
 // // Total component
-const Total = ({ course }) => {
-  const courseParts = course.parts;
+// const Total = ({ course }) => {
+//   const courseParts = course.parts;
 
-  return (
-    <p>
-      <b>
-        Total of{" "}
-        {courseParts.reduce(
-          (accumulator, part) => accumulator + part.exercises,
-          0
-        )}{" "}
-        exercises
-      </b>
-    </p>
-  );
-};
+//   return (
+//     <p>
+//       <b>
+//         Total of{" "}
+//         {courseParts.reduce(
+//           (accumulator, part) => accumulator + part.exercises,
+//           0
+//         )}{" "}
+//         exercises
+//       </b>
+//     </p>
+//   );
+// };
 
 // Course component
-const Course = ({ course }) => {
-  console.log(course);
+const Course = ({ courses }) => {
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      {courses.map((course) => (
+        <Content course={course} />
+      ))}
+      {/* <Header course={course} /> */}
+
+      {/* <Total course={course} /> */}
     </div>
   );
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return <Course courses={courses} />;
 };
 
 export default App;
