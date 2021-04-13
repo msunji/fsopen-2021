5fce5851eb7834306abf55f4c4a2217bf812c572
Marge <marge.consunji@gmail.com>
2021-04-13 15:36:26 +0800

    Starting on exercises 2.11.2-14

---
+++ part2/countries/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const CountryList = ({ queryMatches }) => {
+  console.log(queryMatches);
+  return <p>Results go here</p>;
+};
+
+function App() {
+  // State
+  const [countries, setCountries] = useState([]);
+  const [query, setQuery] = useState("");
+
+  // Effect hook
+  const hook = () => {
+    axios.get("https://restcountries.eu/rest/v2/all").then((res) => {
+      setCountries(res.data);
+    });
+  };
+
+  useEffect(hook, []);
+
+  // handle matching query
+  const queryMatches =
+    query.length === 0
+      ? []
+      : countries.filter((country) =>
+          country.name.toLowerCase().includes(query)
+        );
+
+  // event handlers
+  const handleQuery = (e) => {
+    const inputVal = e.target.value.toLowerCase();
+    setQuery(inputVal);
+  };
+
+  return (
+    <div>
+      find countries: <input value={query} onChange={handleQuery} />
+      <h1>Search Results</h1>
+      <CountryList queryMatches={queryMatches} />
+    </div>
+  );
+}
+
+export default App;
