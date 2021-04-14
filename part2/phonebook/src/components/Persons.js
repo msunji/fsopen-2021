ee6ee60dfe0f0d2a4c58fb6d9d2b3b295e15a6cd
Marge <marge.consunji@gmail.com>
2021-04-14 19:05:41 +0800

    Removed unused axios import

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Person from "./Person";
 
-const Persons = ({ filter, persons, filteredPersons }) => {
+const Persons = ({ filter, persons, filteredPersons, handleDelete }) => {
   return (
     <ul>
       {filter.length === 0
-        ? persons.map((person) => <Person key={person.id} person={person} />)
+        ? persons.map((person) => (
+            <Person
+              key={person.id}
+              person={person}
+              handleDelete={handleDelete}
+            />
+          ))
         : filteredPersons.map((person) => (
-            <Person key={person.id} person={person} />
+            <Person
+              key={person.id}
+              person={person}
+              handleDelete={handleDelete}
+            />
           ))}
     </ul>
   );
