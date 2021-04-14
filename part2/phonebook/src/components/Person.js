133893ddc373554859133fa82973e65bdae179d8
Marge <marge.consunji@gmail.com>
2021-04-14 19:06:05 +0800

    Removed unused axios import

--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -1,19 +1,11 @@
 import React from "react";
-import axios from "axios";
 
-const Person = ({ person }) => {
+const Person = ({ person, handleDelete }) => {
   const { name, number, id } = person;
-  const handleDelete = () => {
-    if (window.confirm(`Delete ${name}?`)) {
-      axios.delete(`http://localhost:3001/persons/${id}`).then((res) => {
-        console.log(res.data);
-      });
-    }
-  };
 
   return (
     <li>
-      {name} {number} <button onClick={handleDelete}>delete</button>
+      {name} {number} <button onClick={() => handleDelete(id)}>delete</button>
     </li>
   );
 };
