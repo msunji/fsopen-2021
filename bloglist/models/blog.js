25be5bf86defcd9e611ebeeb575ba3655bf6d88b
Marge <marge.consunji@gmail.com>
2021-06-21 14:44:01 +0800

    Transform mongoose object remove __v and convert id to string

--- bloglist/models/blog.js
+++ bloglist/models/blog.js
@@ -7,4 +7,13 @@ const blogSchema = new mongoose.Schema({
   likes: Number,
 });
 
+// remove _id and __v from data
+blogSchema.set("toJSON", {
+  transform: (doc, returnedObj) => {
+    returnedObj.id = returnedObj._id.toString();
+    delete returnedObj._id;
+    delete returnedObj.__v;
+  },
+});
+
 module.exports = mongoose.model("Blog", blogSchema);
