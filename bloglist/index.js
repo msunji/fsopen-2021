a31363e28f68ee9d04e1e951a9577d47c548ddfb
Marge <marge.consunji@gmail.com>
2021-06-21 14:07:40 +0800

    Testing to see if add blog function works

--- bloglist/index.js
+++ bloglist/index.js
@@ -41,8 +41,8 @@ app.get("/api/blogs", (req, res) => {
 app.post("/api/blogs", (req, res) => {
   const blog = new Blog(req.body);
 
-  blog.save().then((res) => {
-    res.status(201).json(res);
+  blog.save().then((result) => {
+    res.status(201).json(result);
   });
 });
 
