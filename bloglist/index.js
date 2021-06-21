258ead799b95708dd9bab78712842c913c2a4b77
Marge <marge.consunji@gmail.com>
2021-06-21 13:56:15 +0800

    Setup for exercise

---
+++ bloglist/index.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+const mongoUrl = process.env.MONGODB_URI;
+
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((res) => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((error) => {
+    console.log("Error connecting to DB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/blogs", (req, res) => {
+  Blog.find({}).then((blogs) => {
+    res.json(blogs);
+  });
+});
+
+app.post("/api/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog.save().then((res) => {
+    res.status(201).json(res);
+  });
+});
+
+const PORT = process.env.PORT || 3003;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
